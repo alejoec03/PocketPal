f9d16ec50ee875dff174f8eb3a928a5015c69683
Juan Alejandro Espinosa <65057841+alejoec03@users.noreply.github.com>
2024-06-14 13:53:04 -0500

    vista login

--- PocketPal/PocketPal/urls.py
+++ PocketPal/PocketPal/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from apps.login.views import (home)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # Ruta para la pagina de inicio
+    path('', home, name='home'),
 ]
--- PocketPal/apps/login/views.py
+++ PocketPal/apps/login/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    # Renderiza la plantilla 'index.html' y la devuelve como respuesta
+    return render(request, 'index.html') 
